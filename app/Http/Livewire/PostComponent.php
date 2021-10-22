4f131388c3f69f9ac338a94f9d090b984e008625
Diego Gonzalez <gonzalezdiego1990@gmail.com>
2021-10-22 11:54:00 -0300

    Feat: add pagination bootstrap

---
+++ app/Http/Livewire/PostComponent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Post;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class PostComponent extends Component
+{
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public function render()
+    {
+        return view('livewire.post-component', [
+            'posts' => Post::orderBy('id', 'desc')->paginate(8)
+        ]);
+    }
+}
